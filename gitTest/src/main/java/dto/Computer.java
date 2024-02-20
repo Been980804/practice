fb0ad79a172d870d390dbfd1181cbdcaac967268
admin <admin@DESKTOP-8VOVB0M>
2024-02-20 14:35:30 +0900

    second commit

---
+++ gitTest/src/main/java/dto/Computer.java
@@ -0,0 +1,17 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Computer {
+
+	float cpu;
+	int ram;
+	String gpu;
+	int power;
+	String mainboard;
+	
+	
+}
